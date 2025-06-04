39528839b6f6b90a9280635b77654f36672e8985
Fellipe <fellipe@iftm.edu.br>
2025-06-03 21:55:40 -0300

    Código inicial

--- super-calculadora/main.c
+++ super-calculadora/main.c
@@ -3,6 +3,33 @@
 
 int main()
 {
-    printf("Hello world!\n");
+   int op = 0;
+
+   do {
+   puts("+---==[ OPCOES ]==--+");
+   puts("| 1........Adicao   |");
+   puts("| 2........Subtracao|");
+   puts("| 1........Divisao  |");
+   puts("| 0........Sair     |");
+   puts("+-------------------+");
+   printf(" Opcao: ");
+   scanf("%d", &op);
+
+   switch(op){
+   case 1:
+       // função soma;
+       break;
+   case 2:
+       // fução subtração;
+    break;
+   case 0:
+    break;
+
+   default:
+    puts("\n\n OPCAO INVALIDA!\n\n");
+
+   }
+   }while(op != 0);
+
     return 0;
 }
