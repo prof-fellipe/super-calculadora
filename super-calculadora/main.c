05427c8d029c09e652d0a79690c4e229e9afe080
LuizZX18 <luiz.diniz@estudante.iftm.edu.br>
2025-06-10 22:17:32 -0300

    Update main.c

--- super-calculadora/main.c
+++ super-calculadora/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
+ void Potencia(){
+    int num1,num2, result;
+
+    printf("Digite o numero da base: \n");
+    scanf("%d",&num1);
+    printf("Digite o numero do expoente: \n");
+    scanf("%d",&num2);
+
+    result = pow(num1, num2);
+
+    printf("O resultado eh: %d \n",result);
+
+}
 int main()
 {
    int op = 0;
@@ -9,7 +23,8 @@ int main()
    puts("+---==[ OPCOES ]==--+");
    puts("| 1........Adicao   |");
    puts("| 2........Subtracao|");
-   puts("| 1........Divisao  |");
+   puts("| 3........Divisao  |");
+   puts("| 4........Potencia |");
    puts("| 0........Sair     |");
    puts("+-------------------+");
    printf(" Opcao: ");
@@ -17,10 +32,13 @@ int main()
 
    switch(op){
    case 1:
-       // função soma;
+       // funÃ§Ã£o soma;
        break;
    case 2:
-       // fução subtração;
+       // fuÃ§Ã£o subtraÃ§Ã£o;
+    break;
+   case 4:
+        Potencia();
     break;
    case 0:
     break;
