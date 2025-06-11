7a0024c1a2b549b2ae544e615d10c691c45c4057
Arthur Silva <arthur.barbosa@estudante.iftm.edu.br>
2025-06-10 21:56:31 -0300

    Adicionando opção DELTA

--- super-calculadora/main.c
+++ super-calculadora/main.c
@@ -1,35 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-int main()
-{
-   int op = 0;
+void calcularDelta() {
+    float a, b, c, delta;
 
-   do {
-   puts("+---==[ OPCOES ]==--+");
-   puts("| 1........Adicao   |");
-   puts("| 2........Subtracao|");
-   puts("| 1........Divisao  |");
-   puts("| 0........Sair     |");
-   puts("+-------------------+");
-   printf(" Opcao: ");
-   scanf("%d", &op);
+    printf("Digite os coeficientes a, b e c da equaÃ§Ã£o do segundo grau (ax^2 + bx + c = 0):\n");
+    printf("a: ");
+    scanf("%f", &a);
+    printf("b: ");
+    scanf("%f", &b);
+    printf("c: ");
+    scanf("%f", &c);
 
-   switch(op){
-   case 1:
-       // função soma;
-       break;
-   case 2:
-       // fução subtração;
-    break;
-   case 0:
-    break;
+    if (a == 0) {
+        printf("Isso nÃ£o Ã© uma equaÃ§Ã£o do segundo grau.\n");
+        return;
+    }
 
-   default:
-    puts("\n\n OPCAO INVALIDA!\n\n");
+    delta = b * b - 4 * a * c;
+    printf("Delta (Î”) = %.2f\n", delta);
 
-   }
-   }while(op != 0);
+    if (delta < 0) {
+        printf("NÃ£o existem raÃ­zes reais.\n");
+    } else if (delta == 0) {
+        float raiz = -b / (2 * a);
+        printf("Existe uma raiz real: x = %.2f\n", raiz);
+    } else {
+        float raiz1 = (-b + sqrt(delta)) / (2 * a);
+        float raiz2 = (-b - sqrt(delta)) / (2 * a);
+        printf("Existem duas raÃ­zes reais:\n");
+        printf("x1 = %.2f\n", raiz1);
+        printf("x2 = %.2f\n", raiz2);
+    }
+}
+
+int main() {
+    int op = 0;
+
+    do {
+        puts("+---==[ OPCOES ]==--+");
+        puts("| 1........Adicao   |");
+        puts("| 2........Subtracao|");
+        puts("| 3........Divisao  |");
+        puts("| 19.......Delta    |");
+        puts("| 0........Sair     |");
+        puts("+-------------------+");
+        printf(" Opcao: ");
+        scanf("%d", &op);
+
+        switch(op) {
+        case 1:
+
+            break;
+
+        case 2:
+
+            break;
+
+        case 3:
+
+            break;
+
+        case 19:
+            calcularDelta();
+            break;
+
+        case 0:
+            printf("Saindo do programa...\n");
+            break;
+
+        default:
+            puts("\n\n OPCAO INVALIDA!\n\n");
+        }
+
+        printf("\n");
+
+    } while(op != 0);
 
     return 0;
 }
