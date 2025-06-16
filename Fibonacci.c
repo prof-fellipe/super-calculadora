b54c67c3b90959d021ac5a62512e6b9ebd4588d5
jackelineneres <jackeline.jesus@estudante.iftm.edu.br>
2025-06-16 20:33:21 -0300

    Update and rename main.c to Fibonacci.c

--- Fibonacci.c
+++ Fibonacci.c
@@ -6,10 +6,10 @@ int main() {
     setlocale (LC_ALL, "Portuguese");
     int n, primeiro = 0, segundo = 1, proximo, i;
 
-    printf("Digite o número de termos da sequência de Fibonacci: ");
+    printf("Digite o nÃºmero de termos da sequÃªncia de Fibonacci: ");
     scanf("%d", &n);
 
-    printf("Sequência de Fibonacci com %d termos:\n", n);
+    printf("SequÃªncia de Fibonacci com %d termos:\n", n);
 
     for (i = 0; i < n; i++) {
         if (i <= 1)
